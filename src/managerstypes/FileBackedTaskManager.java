073832a6219d1ac01fc570024df4714f4f638e69
Ivan Baturo <baturo.ivan@bk.ru>
2025-01-29 21:51:39 +0300

    feat: new FileBackedTaskManager class and overrided mod.methods

---
+++ src/managerstypes/FileBackedTaskManager.java
@@ -0,0 +1,96 @@
+package managerstypes;
+
+import taskstypes.Epic;
+import taskstypes.StatusOfTask;
+import taskstypes.SubTask;
+import taskstypes.Task;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+
+public class FileBackedTaskManager extends InMemoryTaskManager{
+    private final File file;
+
+    public FileBackedTaskManager(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void addTask(Task task) {
+        super.addTask(task);
+        save();
+    }
+
+    @Override
+    public void addEpic(Epic epic) {
+        super.addEpic(epic);
+        save();
+    }
+
+    @Override
+    public void addSubTask(SubTask subtask) {
+        super.addSubTask(subtask);
+        save();
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void updateSubTask(SubTask subtask) {
+        super.updateSubTask(subtask);
+        save();
+    }
+
+    @Override
+    public void removeTask(int id) {
+        super.removeTask(id);
+        save();
+    }
+
+    @Override
+    public void removeEpic(int id) {
+        super.removeEpic(id);
+        save();
+    }
+
+    @Override
+    public void removeSubTask(int id) { //
+        super.removeSubTask(id);
+        save();
+    }
+
+    @Override
+    public void removeAllTasks() {
+        super.removeAllTasks();
+        save();
+    }
+
+    @Override
+    public void removeAllEpics() {
+        super.removeAllEpics();
+        save();
+    }
+
+    @Override
+    public void removeAllSubTasks() {
+        super.removeAllSubTasks();
+        save();
+    }
+
+    public void save(){
+
+    }
+
+}
