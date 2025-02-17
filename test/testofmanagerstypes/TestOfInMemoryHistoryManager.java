79726a29df72724f9a3013b9331bec2b5bae94fc
Ivan Baturo <baturo.ivan@bk.ru>
2025-02-17 14:05:50 +0300

    feat: added new test classes plus minor fixes

--- test/testofmanagerstypes/TestOfInMemoryHistoryManager.java
+++ test/testofmanagerstypes/TestOfInMemoryHistoryManager.java
@@ -5,6 +5,7 @@ import managerstypes.Managers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import taskstypes.Epic;
+import taskstypes.StatusOfTask;
 import taskstypes.Task;
 import taskstypes.SubTask;
 
@@ -23,9 +24,9 @@ public class TestOfInMemoryHistoryManager {
 
     @Test
     public void shouldAddAndGetHistory() {
-        Task task1 = new Task("Задача1","тема задачи 1");
-        Epic epic1 = new Epic("Эпик1", "Описание эпика1");
-        SubTask subtask1 = new SubTask("Подзадача1", "Описание подзадачи1", epic1.getId());
+        Task task1 = new Task("Задача1","тема задачи 1", StatusOfTask.NEW, 0);
+        Epic epic1 = new Epic("Эпик1", "Описание эпика1", 1);
+        SubTask subtask1 = new SubTask("Подзадача1", "Описание подзадачи1", StatusOfTask.NEW, 2, epic1.getId());
         inMemoryHistoryManager.addToHistory(task1);
         inMemoryHistoryManager.addToHistory(epic1);
         inMemoryHistoryManager.addToHistory(subtask1);
--- test/testoftaskstypes/EpicTest.java
+++ test/testoftaskstypes/EpicTest.java
@@ -1,13 +1,27 @@
 package testoftaskstypes;
 
+import managerstypes.Managers;
+import managerstypes.TaskManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import taskstypes.Epic;
+import taskstypes.StatusOfTask;
+import taskstypes.SubTask;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class EpicTest {
 
+    private TaskManager manager;
+    private Epic epic;
+
+    @BeforeEach
+    void setUp() {
+        manager = Managers.getDefault();
+        epic = new Epic("Эпик1", "тема эпика 1");
+        manager.addEpic(epic);
+    }
 
     @Test
     void shouldReturnTrueIfIdsAreEquals() {
@@ -18,10 +32,51 @@ public class EpicTest {
         System.out.println(epic2.getId());
     }
     @Test
-    void TestOfPoint3() {
+    void shouldNotAddEpicHimselfForItsOwnSubtasks() {
         Epic epic1 = new Epic("Эпик2", "тема эпика 2", 2);
         epic1.addSubtask(epic1.getId());
         assertNull(epic1.getSubTasksIds());
+    }
+
+    @Test
+    void shouldBeNewIfHasNoSubtasks() {
+        assertEquals(StatusOfTask.NEW, epic.getStatus());
+    }
+
+    @Test
+    void shouldBeNewIfAllSubtasksNew() {
+        SubTask subTask1 = new SubTask("подЗадача1", "тема подзадачи 1", StatusOfTask.NEW, 2, epic.getId(), null, null);
+        SubTask subTask2 = new SubTask("подЗадача2", "тема подзадачи 2", StatusOfTask.NEW, 3, epic.getId(), null, null);
+        manager.addSubTask(subTask1);
+        manager.addSubTask(subTask2);
+        assertEquals(StatusOfTask.NEW, epic.getStatus());
+    }
+
+    @Test
+    void shouldBeDoneIfAllSubtasksDone() {
+        SubTask subTask1 = new SubTask("Подзадача1", "тема подзадачи 1", StatusOfTask.DONE, 2, epic.getId(), null, null);
+        SubTask subTask2 = new SubTask("Подзадача2", "тема подзадачи 2", StatusOfTask.DONE, 3, epic.getId(), null, null);
+        manager.addSubTask(subTask1);
+        manager.addSubTask(subTask2);
+        assertEquals(StatusOfTask.DONE, epic.getStatus());
+    }
+
+    @Test
+    void shouldBeInProgressIfSubtasksAreNewAndDone() {
+        SubTask subTask1 = new SubTask("Подзадача1", "тема подзадачи 1", StatusOfTask.NEW, 2, epic.getId(), null, null);
+        SubTask subTask2 = new SubTask("Подзадача2", "тема подзадачи 2", StatusOfTask.DONE, 3, epic.getId(), null, null);
+        manager.addSubTask(subTask1);
+        manager.addSubTask(subTask2);
+        assertEquals(StatusOfTask.IN_PROGRESS, epic.getStatus());
+    }
+
+    @Test
+    void shouldBeInProgressIfSubtasksInProgress() {
+        SubTask subTask1 = new SubTask("Подзадача1", "тема подзадачи 1", StatusOfTask.IN_PROGRESS, 2, epic.getId(), null, null);
+        SubTask subTask2 = new SubTask("Подзадача2", "тема подзадачи 2", StatusOfTask.IN_PROGRESS, 3, epic.getId(), null, null);
+        manager.addSubTask(subTask1);
+        manager.addSubTask(subTask2);
+        assertEquals(StatusOfTask.IN_PROGRESS, epic.getStatus());
     }
 
 }
